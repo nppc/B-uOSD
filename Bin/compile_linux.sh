d005143cc173ede3ccbef794098bb67559a43a17
Palonen <ppalonen@bethel.jw.org>
2017-06-28 15:42:56 +0200

    change compile scripts

--- Bin/compile_linux.sh
+++ Bin/compile_linux.sh
@@ -2,13 +2,13 @@
 
 cd ../VmOSD/VmOSD/
 avra -D SYMBOL_NORMAL -D BITMAP_COPTER main.asm
-mv main.hex ../../Bin/B-uOSD_NC_v11.hex
+mv main.hex ../../Bin/B-uOSD_NC_v12.hex
 avra -D SYMBOL_NORMAL -D BITMAP_GOOGLES main.asm
-mv main.hex ../../Bin/B-uOSD_NG_v11.hex
+mv main.hex ../../Bin/B-uOSD_NG_v12.hex
 avra -D SYMBOL_DOUBLE -D BITMAP_COPTER main.asm
-mv main.hex ../../Bin/B-uOSD_DC_v11.hex
+mv main.hex ../../Bin/B-uOSD_DC_v12.hex
 avra -D SYMBOL_DOUBLE -D BITMAP_GOOGLES main.asm
-mv main.hex ../../Bin/B-uOSD_DG_v11.hex
+mv main.hex ../../Bin/B-uOSD_DG_v12.hex
 rm main.cof
 rm main.eep.hex
 rm main.obj
--- Bin/compile_windows.cmd
+++ Bin/compile_windows.cmd
@@ -1,7 +1,7 @@
 @SET AVRpath="C:\Program Files (x86)"\Atmel\Studio\7.0\toolchain\avr8\avrassembler\
 @cd ..\VmOSD\VmOSD\
-%AVRpath%\avrasm2.exe -D SYMBOL_NORMAL -D BITMAP_COPTER -o ../../Bin/B-uOSD_NC_v11.hex -fI main.asm
-%AVRpath%\avrasm2.exe -D SYMBOL_NORMAL -D BITMAP_GOOGLES -o ../../Bin/B-uOSD_NG_v11.hex -fI main.asm
-%AVRpath%\avrasm2.exe -D SYMBOL_DOUBLE -D BITMAP_COPTER -o ../../Bin/B-uOSD_DC_v11.hex -fI main.asm
-%AVRpath%\avrasm2.exe -D SYMBOL_DOUBLE -D BITMAP_GOOGLES -o ../../Bin/B-uOSD_DG_v11.hex -fI main.asm
+%AVRpath%\avrasm2.exe -D SYMBOL_NORMAL -D BITMAP_COPTER -o ../../Bin/B-uOSD_NC_v12.hex -fI main.asm
+%AVRpath%\avrasm2.exe -D SYMBOL_NORMAL -D BITMAP_GOOGLES -o ../../Bin/B-uOSD_NG_v12.hex -fI main.asm
+%AVRpath%\avrasm2.exe -D SYMBOL_DOUBLE -D BITMAP_COPTER -o ../../Bin/B-uOSD_DC_v12.hex -fI main.asm
+%AVRpath%\avrasm2.exe -D SYMBOL_DOUBLE -D BITMAP_GOOGLES -o ../../Bin/B-uOSD_DG_v12.hex -fI main.asm
 @cd ..\..\Bin
